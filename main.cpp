ee968647e1dc8f2845bc02e91ddde72adac7dbff
bring <bringonthepretty@gmail.com>
2021-02-05 20:42:45 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include "cmath"
+#include <chrono>
 
 using namespace std;
 
 void setUp();
 bool isNumAppropriate(vector<int> key);
-void gameLoop();
+vector<int> checkNum(vector<int> numToCheck, vector<int> key);
+int arrayToNum(vector<int> key);
+void firstPlayerTurn();
+void secondPlayerTurn();
 
 vector<int> firstPlayerSavedNum = vector<int>();
 vector<int> secondPlayerSavedNum = vector<int>();
-vector<int> firstPlayerSuggestedNums = vector<int>();
-vector<int> secondPlayerSuggestedNums = vector<int>();
+vector<vector<int>> firstPlayerSuggestedNums = vector<vector<int>>();
+vector<vector<int>> secondPlayerSuggestedNums = vector<vector<int>>();
 vector<int> bufferNum = vector<int>();
-bool isGameRunning = true;
 
 int main() {
     setUp();
 
-    while (isGameRunning){
-        gameLoop();
+    if (clock() % 2 == 0){
+        firstPlayerTurn();
+    } else {
+        secondPlayerTurn();
     }
 
     return 0;
@@ -36,6 +40,10 @@ void setUp(){
         cout << "Enter 4-digit number with unique numerals" << "\n";
         cin >> buf;
 
+        if (buf < 0){
+            continue;
+        }
+
         int length = to_string(buf).length();
 
         for (int i = 0; i < length; ++i) {
@@ -46,9 +54,9 @@ void setUp(){
     } while (!isNumAppropriate(firstPlayerSavedNum));
 
     reverse(firstPlayerSavedNum.begin(), firstPlayerSavedNum.end());
-    for (int i : firstPlayerSavedNum) {
-        cout << i;
-    }
+//    for (int i : firstPlayerSavedNum) {
+//        cout << i;
+//    }
     cout << endl;
 
     cout << "Second Player:" << "\n";
@@ -57,6 +65,10 @@ void setUp(){
         secondPlayerSavedNum.clear();
         cout << "Enter 4-digit number with unique numerals" << "\n";
         cin >> buf;
+
+        if (buf < 0){
+            continue;
+        }
 
         int length = to_string(buf).length();
 
@@ -67,9 +79,9 @@ void setUp(){
     } while (!isNumAppropriate(secondPlayerSavedNum));
 
     reverse(secondPlayerSavedNum.begin(), secondPlayerSavedNum.end());
-    for (int i : secondPlayerSavedNum) {
-        cout << i;
-    }
+//    for (int i : secondPlayerSavedNum) {
+//        cout << i;
+//    }
     cout << endl;
 }
 
@@ -97,13 +109,112 @@ bool isNumAppropriate(vector<int> key){
     return  flag;
 }
 
-void gameLoop(){
+void firstPlayerTurn(){
+    vector<int> resultArray;
 
     cout << "First Player Turn:" << "\n";
     cout << "\t" << "Previously Suggested Numbers:" << "\n";
-    for (int i : firstPlayerSuggestedNums) {
-        cout << firstPlayerSuggestedNums.at(i) << "\n";
+
+    for (int i = 0; i < firstPlayerSuggestedNums.size(); i++) {
+        cout << "\t" << "\t" << firstPlayerSuggestedNums.at(i).at(0) << "   Amount Of Cows: " << firstPlayerSuggestedNums.at(i).at(1) << "   Amount Of Bulls: " << firstPlayerSuggestedNums.at(i).at(2) << "\n";
     }
 
-    cout << "\t" << "Suggest New Number" << "\n";
+    int buf;
+    do {
+        bufferNum.clear();
+        cout << "\t" << "Suggest New 4-digit Number" << "\n";
+        cin >> buf;
+
+        int length = to_string(buf).length();
+
+        for (int i = 0; i < length; ++i) {
+            bufferNum.push_back(buf % 10);
+            buf = buf / 10;
+        }
+    } while (!isNumAppropriate(bufferNum));
+
+    reverse(bufferNum.begin(), bufferNum.end());
+
+    resultArray.clear();
+    resultArray = checkNum(secondPlayerSavedNum, bufferNum);
+
+    firstPlayerSuggestedNums.push_back(vector<int>{arrayToNum(bufferNum), resultArray.at(0) - resultArray.at(1), resultArray.at(1)});
+
+    cout << "\t" << "Amount Of Cows: " << resultArray.at(0) - resultArray.at(1) << "   Amount Of Bulls: " << resultArray.at(1) << "\n";
+
+    if (resultArray.at(1) == 4){
+        cout << "FIRST PLAYER WINS";
+    } else {
+        secondPlayerTurn();
+    }
+
+}
+
+void secondPlayerTurn(){
+    vector<int> resultArray;
+
+    cout << "Second Player Turn:" << "\n";
+    cout << "\t" << "Previously Suggested Numbers:" << "\n";
+
+    for (int i = 0; i < secondPlayerSuggestedNums.size(); i++) {
+        cout << "\t" << "\t" << secondPlayerSuggestedNums.at(i).at(0) << "   Amount Of Cows: " << secondPlayerSuggestedNums.at(i).at(1) << "   Amount Of Bulls: " << secondPlayerSuggestedNums.at(i).at(2) << "\n";
+    }
+
+    int buf;
+    do {
+        bufferNum.clear();
+        cout << "\t" << "Suggest New 4-digit Number" << "\n";
+        cin >> buf;
+
+        int length = to_string(buf).length();
+
+        for (int i = 0; i < length; ++i) {
+            bufferNum.push_back(buf % 10);
+            buf = buf / 10;
+        }
+    } while (!isNumAppropriate(bufferNum));
+
+    reverse(bufferNum.begin(), bufferNum.end());
+
+    resultArray.clear();
+    resultArray = checkNum(firstPlayerSavedNum, bufferNum);
+
+    secondPlayerSuggestedNums.push_back(vector<int>{arrayToNum(bufferNum), resultArray.at(0) - resultArray.at(1), resultArray.at(1)});
+
+    cout << "\t" << "Amount Of Cows: " << resultArray.at(0) - resultArray.at(1) << "   Amount Of Bulls: " << resultArray.at(1) << "\n";
+
+    if (resultArray.at(1) == 4){
+        cout << "SECOND PLAYER WINS";
+    } else {
+        firstPlayerTurn();
+    }
+
+}
+
+vector<int> checkNum(vector<int> numToCheck, vector<int> key){
+    vector<int> result = {0, 0};
+
+    for (int i = 0; i < numToCheck.size(); ++i) {
+        for (int j = 0; j < key.size(); ++j) {
+            if (numToCheck.at(i) == key.at(j)){
+                result.at(0)++;
+                if (i == j){
+                    result.at(1)++;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int arrayToNum(vector<int> key){
+    int result = 0;
+
+    result += key.at(3);
+    result += key.at(2) * 10;
+    result += key.at(1) * 100;
+    result += key.at(0) * 1000;
+
+    return result;
 }
